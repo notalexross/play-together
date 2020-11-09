58f15db02c2a38999b3c17436d9ae8d316fdd8c7
Daniel Ross <69479756+notalexross@users.noreply.github.com>
2020-11-09 22:37:06 +0000

    feat: update accordion border styling

--- src/components/accordion/styles/index.js
+++ src/components/accordion/styles/index.js
@@ -6,22 +6,28 @@ export const Container = styled.section`
   background: #252525;
   border: solid 1px;
   width: 300px;
+  height: 500px;
 `;
 
-export const Item = styled.div``;
+export const Item = styled.div`
+  border-top: solid 1px #e5e5e533;
+  &:last-of-type {
+    border-bottom: solid 1px #e5e5e533;
+  }
+`;
 
 export const Header = styled.div`
   display: flex;
   align-items: center;
   cursor: pointer;
   user-select: none;
-  border-top: solid 1px;
 `;
 
 export const Body = styled.div`
   user-select: none;
   padding: 0.25em 1em 0.5em 1em;
-  background: #1e1e1e
+  background: #1e1e1e;
+  border-top: solid 1px #e5e5e533;
 `;
 
 export const IconWrapper = styled.div`
