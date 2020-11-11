de78f967a3132cb1ccc1e343b1d133067a30971f
Daniel Ross <69479756+notalexross@users.noreply.github.com>
2020-11-11 01:04:45 +0000

    feat: comment user context to prevent timeouts

--- src/context/user.js
+++ src/context/user.js
@@ -8,52 +8,54 @@ const { Provider } = context
 function ContextProvider({ children }) {
   const [ userData, setUserData ] = useState(JSON.parse(localStorage.getItem('userData')))
   const [ shouldUpdateUserData, setShouldUpdateUserData ] = useState(true)
-  const [ isLoading, setIsLoading ] = useState(true)
+  // const [ isLoading, setIsLoading ] = useState(true)
+
+  // useEffect(() => {
+  //   if (shouldUpdateUserData) {
+
+  //     let action
+  //     if (userData && userData.id) {
+  //       action = { type: 'UPDATE_USER', payload: userData }
+  //     } else {
+  //       action = { type: 'CREATE_USER', payload: userData }
+  //     }
+
+  //     const options = {
+  //       method: 'POST',
+  //       body: JSON.stringify(action),
+  //       headers: {
+  //         'Content-Type': 'application/json'
+  //       },
+  //     }
+
+  //     fetch(`${SERVER_URL}/users/`, options)
+  //       .then(response => {
+  //         if (!response.ok) throw new Error(response.status) // this wont actually show the message sent back
+  //         return response.json()
+  //       })
+  //       .then(data => {
+  //         setUserData(userData => {
+  //           const newUserData = { ...userData, ...data }
+  //           console.log(newUserData)
+  //           localStorage.setItem('userData', JSON.stringify(newUserData))
+  //           return { ...userData, ...data }
+  //         })
+  //         setShouldUpdateUserData(false)
+          
+  //       })
+  //       .catch(e => console.error(e))
+  //       .finally(() => setIsLoading(false))   
+  //   }
+
+  // }, [shouldUpdateUserData])
+
+  // TODO
+  const isLoading = false;
 
   const setNickname = newNickname => {
     setUserData(user => ({ ...user, nickname: newNickname }))
     setShouldUpdateUserData(true)
   }
-
-
-  useEffect(() => {
-    if (shouldUpdateUserData) {
-
-      let action
-      if (userData && userData.id) {
-        action = { type: 'UPDATE_USER', payload: userData }
-      } else {
-        action = { type: 'CREATE_USER', payload: userData }
-      }
-
-      const options = {
-        method: 'POST',
-        body: JSON.stringify(action),
-        headers: {
-          'Content-Type': 'application/json'
-        },
-      }
-
-      fetch(`${SERVER_URL}/users/`, options)
-        .then(response => {
-          if (!response.ok) throw new Error(response.status) // this wont actually show the message sent back
-          return response.json()
-        })
-        .then(data => {
-          setUserData(userData => {
-            const newUserData = { ...userData, ...data }
-            console.log(newUserData)
-            localStorage.setItem('userData', JSON.stringify(newUserData))
-            return { ...userData, ...data }
-          })
-          setShouldUpdateUserData(false)
-          
-        })
-        .catch(e => console.error(e))
-        .finally(() => setIsLoading(false))   
-    }
-
-  }, [shouldUpdateUserData])
   
   return (
     <Provider value={{ ...userData, setNickname }} >
